eb8d383d20e43991376092bed4356f7eec91b68c
Jeremiah Lewis <4462211+jeremiahpslewis@users.noreply.github.com>
2022-07-07 15:36:20 +0200

    Add new query

--- src/esef_xml_parse.jl
+++ src/esef_xml_parse.jl
@@ -5,8 +5,12 @@ using HTTP
 using JSON
 using DataFrames
 using DelimitedFiles
+using UUIDs
+using Arrow
 
-df, df_error = ESEF.get_esef_xbrl_filings()
+include("oxigraph_server.jl")
+
+
 
 function pluck_xbrl_json(url)
     r = HTTP.get(url)
@@ -36,13 +40,6 @@ function pluck_xbrl_json(url)
     return finished_facts
 end
 
-df = @chain df begin
-    @subset(:xbrl_json_path != nothing)
-    @transform(:xbrl_json_url = "https://filings.xbrl.org/" * :filing_key * "/" * HTTP.escapeuri(:xbrl_json_path))
-    @select(:xbrl_json_url)
-end
-
-
 
 function rec_flatten_dict(d, prefix_delim = ".")
     # Source: https://discourse.julialang.org/t/question-is-there-a-function-to-flatten-a-nested-dictionary/54462/2
@@ -60,21 +57,67 @@ function rec_flatten_dict(d, prefix_delim = ".")
     return new_d
 end
 
-df_facts = DataFrame()
 
-for r in eachrow(df)
-    df_ = pluck_xbrl_json(r[:xbrl_json_url])
-    df_rdf = @chain df_ begin
-        @transform(:rdf_line = "<http://example.org/" * HTTP.escapeuri(:subject) * "> <http://example.org/" * HTTP.escapeuri(:predicate) * "> <http://example.org/" * HTTP.escapeuri(:object) * "> .")
-        @select(:rdf_line)
+function export_concept_count_table()
+    query_item_types = """
+        # Get count of all 'concepts' included in ESEF dataset
+        SELECT (str(?obj) as ?obj_1)  (str(COUNT(?obj)) as ?obj_count) WHERE {
+        ?sub <http://example.org/dimensions.concept> ?obj .
+        } GROUP BY ?obj
+        ORDER BY DESC(?obj_count)
+        LIMIT 1000000
+    """
+
+    query_response = @chain query_item_types sparql_query
+
+    DataFrame(query_response["results"]["bindings"])
+
+    df_concepts = DataFrame(concept = String[], frequency = Int[])
+
+    for i in query_response["results"]["bindings"]
+        push!(df_concepts, [HTTP.unescapeuri(replace(i["obj_1"]["value"], "http://example.org/" => "")), parse(Int, i["obj_count"]["value"])])
     end
-    append!(df_facts, df_rdf)
+
+
+    return df_concepts
 end
 
+function process_xbrl_filings()
 
-open("oxigraph_rdf.nt", "w") do io
-    writedlm(io, df_facts[:, :rdf_line])
+    df, df_error = ESEF.get_esef_xbrl_filings()
+
+    df = @chain df begin
+        @subset(:xbrl_json_path != nothing)
+        @transform(:xbrl_json_url = "https://filings.xbrl.org/" * :filing_key * "/" * HTTP.escapeuri(:xbrl_json_path))
+        @select(:xbrl_json_url)
+    end
+
+    df_facts = DataFrame()
+
+    for r in eachrow(df)
+        df_ = pluck_xbrl_json(r[:xbrl_json_url])
+        df_rdf = @chain df_ begin
+            # TODO: Rethink normalization, instead of using uuid for facts at RDF subject field
+            @transform(:rdf_line = "<http://example.org/" * string(uuid4()) * "> <http://example.org/" * HTTP.escapeuri(:predicate) * "> <http://example.org/" * HTTP.escapeuri(:object) * "> .")
+            @select(:rdf_line)
+        end
+        append!(df_facts, df_rdf)
+    end
+
+    rm("oxigraph_rdf.nt")
+
+    open("oxigraph_rdf.nt", "w") do io
+        writedlm(io, df_facts[:, :rdf_line])
+    end
+
+
+    oxigraph_process = serve_oxigraph(; nt_file_path = "oxigraph_rdf.nt", keep_open = true)
+
+    # Rollup of all concepts available from ESEF data using XBRL's filings API
+    df_concepts = export_concept_count_table()
+    Arrow.write("concept_df.arrow", df_concepts)
+
+    # table = Arrow.Table("concept_df.arrow")
+
+    kill(oxigraph_process)
 end
-
-
-serve_oxigraph(; nt_file_path = "oxigraph_rdf.nt")
--- src/oxigraph_server.jl
+++ src/oxigraph_server.jl
@@ -1,8 +1,26 @@
 using HTTP
 using JSON
 using Chain
+using Arrow
 
-function serve_oxigraph(; nt_file_path = "")
+function sparql_query(query)
+    headers = ["Content-Type" => "application/sparql-query", "Accept" => "application/sparql-results+json"]
+    url = "http://localhost:7878/query"
+    
+    r = HTTP.request("POST", url, headers, query)
+
+    # Check 200 HTTP status code
+    @assert(r.status == 200)
+
+    raw_data = @chain r.body begin
+        String()
+        JSON.parse()
+    end
+    
+    return raw_data
+end
+
+function serve_oxigraph(; nt_file_path = "", keep_open = false)
     # 1. Install oxigraph server
     run(`cargo install oxigraph_server`)
 
@@ -14,27 +32,16 @@ function serve_oxigraph(; nt_file_path = "")
     end
 
     # 2. Load data into database
-    run(`oxigraph_server --location esef_oxigraph_data load --file $nt_file_path`)
+    run(`$(ENV["HOME"])/.cargo/bin/oxigraph_server --location esef_oxigraph_data load --file $nt_file_path`)
 
     # 3. Spin up database
-    oxigraph_process = run(`oxigraph_server --location esef_oxigraph_data serve`; wait=false)
+    oxigraph_process = run(`$(ENV["HOME"])/.cargo/bin/oxigraph_server --location esef_oxigraph_data serve`; wait=false)
 
     try
         # 4. Query database
-        headers = ["Content-Type" => "application/sparql-query", "Accept" => "application/sparql-results+json"]
-        url = "http://localhost:7878/query"
-        query = "SELECT (COUNT(*) as ?count) WHERE { ?s ?p ?o } LIMIT 10"
-        r = HTTP.request("POST", url, headers, query)
+        query_response = @chain "SELECT (COUNT(*) as ?count) WHERE { ?s ?p ?o } LIMIT 10" sparql_query
 
-        # Check 200 HTTP status code
-        @assert(r.status == 200)
-
-        raw_data = @chain r.body begin
-            String()
-            JSON.parse()
-        end
-
-        n_items = @chain raw_data["results"]["bindings"][1]["count"]["value"] parse(Int64, _)
+        n_items = @chain query_response["results"]["bindings"][1]["count"]["value"] parse(Int64, _)
 
         # 5. Check that we got the right number of items
         @assert n_items == countlines(nt_file_path)
@@ -42,6 +49,13 @@ function serve_oxigraph(; nt_file_path = "")
         kill(oxigraph_process)
     finally
         # 6. Stop database
-        kill(oxigraph_process)
+        if keep_open
+            return oxigraph_process
+        else
+            kill(oxigraph_process)
     end
 end
+
+
+
+
