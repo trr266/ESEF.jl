c3b3a702361cba40756c1b95465e7f26eb71cba8
Jeremiah Lewis <4462211+jeremiahpslewis@users.noreply.github.com>
2022-09-28 15:22:11 +0200

    Format code

--- src/esef_index_viz.jl
+++ src/esef_index_viz.jl
@@ -27,7 +27,6 @@ function get_esef_mandate_df()
     return esef_year_df
 end
 
-
 function generate_esef_basemap()
     url = "https://raw.githubusercontent.com/nvkelso/natural-earth-vector/master/geojson/"
     country = Downloads.download(url * "ne_50m_admin_0_countries.geojson")
@@ -38,8 +37,14 @@ function generate_esef_basemap()
 
     mandate_df = get_esef_mandate_df()
 
-    malta = [c for c in tiny_country_json["features"] if c["properties"]["ADMIN"] == "Malta"]
-    europe = [c for c in country_json["features"] if (c["properties"]["ADMIN"] ∈ mandate_df[!, :Country]) & (c["properties"]["ADMIN"] != "Malta")]
+    malta = [
+        c for c in tiny_country_json["features"] if c["properties"]["ADMIN"] == "Malta"
+    ]
+    europe = [
+        c for c in country_json["features"] if
+        (c["properties"]["ADMIN"] ∈ mandate_df[!, :Country]) &
+        (c["properties"]["ADMIN"] != "Malta")
+    ]
     country_json["features"] = [malta..., europe...]
 
     country_geo = GeoJSON.read(JSON.json(country_json))
@@ -48,24 +53,24 @@ end
 
 function generate_esef_report_map()
     background_gray = RGBf(0.85, 0.85, 0.85)
-    fontsize_theme = Theme(fontsize = 20,  backgroundcolor = background_gray)
+    fontsize_theme = Theme(; fontsize=20, backgroundcolor=background_gray)
     set_theme!(fontsize_theme)
     dest = "+proj=laea"
     source = "+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs"
 
-    fig = Figure(resolution = (1000,500))
+    fig = Figure(; resolution=(1000, 500))
     gd = fig[1, 1] = GridLayout()
 
     ga = GeoAxis(
         gd[1, 1];
-        source = source,
-        dest = dest,
+        source=source,
+        dest=dest,
         lonlims=(-28, 35),
-        latlims = (35, 72),
+        latlims=(35, 72),
         title="ESEF Reports Availability by Country",
-        subtitle = "(XBRL Repository)",
-        backgroundcolor = background_gray,
-        )
+        subtitle="(XBRL Repository)",
+        backgroundcolor=background_gray,
+    )
 
     eu_geojson = generate_esef_basemap()
 
@@ -76,24 +81,35 @@ function generate_esef_report_map()
     end
 
     report_count_vect = map(eu_geojson) do geo
-        report_count = (@chain country_rollup @subset(:country == geo.ADMIN) @select(:report_count))
+        report_count = (@chain country_rollup @subset(:country == geo.ADMIN) @select(
+            :report_count
+        ))
         nrow(report_count) > 0 ? report_count[1, 1] : missing
     end
 
     max_reports = maximum(country_rollup[!, :report_count])
-    color_scale_ = range(parse(Colorant, "#ffffff"), parse(Colorant, "#ffb43b"), max_reports+1)
+    color_scale_ = range(
+        parse(Colorant, "#ffffff"), parse(Colorant, "#ffb43b"), max_reports + 1
+    )
     # NOTE: Work around for `ERROR: MethodError: no method matching MultiPolygon(::Point{2, Float32})`
     for (c, report_count) in zip(eu_geojson, report_count_vect)
-        poly!(ga, GeoMakie.geo2basic(c);
-            strokecolor = RGBf(0.90, 0.90, 0.90),
-            strokewidth = 1,
-            color= color_scale_[report_count],
-            label="test"
+        poly!(
+            ga,
+            GeoMakie.geo2basic(c);
+            strokecolor=RGBf(0.90, 0.90, 0.90),
+            strokewidth=1,
+            color=color_scale_[report_count],
+            label="test",
         )
     end
 
-
-    cbar = Colorbar(gd[1,2]; colorrange = (0, max_reports), colormap = color_scale_, label = "ESEF Reports (all-time, per country)", height = Relative(0.65))
+    cbar = Colorbar(
+        gd[1, 2];
+        colorrange=(0, max_reports),
+        colormap=color_scale_,
+        label="ESEF Reports (all-time, per country)",
+        height=Relative(0.65),
+    )
 
     hidedecorations!(ga)
     hidespines!(ga)
@@ -108,47 +124,50 @@ end
 
 function generate_esef_mandate_map()
     background_gray = RGBf(0.85, 0.85, 0.85)
-    fontsize_theme = Theme(fontsize = 20,  backgroundcolor = background_gray)
+    fontsize_theme = Theme(; fontsize=20, backgroundcolor=background_gray)
     set_theme!(fontsize_theme)
     dest = "+proj=laea"
     source = "+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs"
 
-    fig = Figure(resolution = (1000,500))
+    fig = Figure(; resolution=(1000, 500))
     gd = fig[1, 1] = GridLayout()
 
     ga = GeoAxis(
         gd[1, 1];
-        source = source,
-        dest = dest,
+        source=source,
+        dest=dest,
         lonlims=(-28, 35),
-        latlims = (35, 72),
+        latlims=(35, 72),
         title="ESEF Mandate by Country",
-        subtitle = "(Based on Issuer's Fiscal Year Start Date)",
-        backgroundcolor = background_gray,
-        )
+        subtitle="(Based on Issuer's Fiscal Year Start Date)",
+        backgroundcolor=background_gray,
+    )
 
     eu_geojson = generate_esef_basemap()
 
     esef_year_df = get_esef_mandate_df()
 
     mandate_year_vect = map(eu_geojson) do geo
-        mandate_year = (@chain esef_year_df @subset(:Country == geo.ADMIN) @select(:Mandate_Affects_Fiscal_Year_Beginning))
+        mandate_year = (@chain esef_year_df @subset(:Country == geo.ADMIN) @select(
+            :Mandate_Affects_Fiscal_Year_Beginning
+        ))
         mandate_year[1, 1]
     end
-
 
     color_scale_ = parse.((Colorant,), trr_266_colors)
     # NOTE: Work around for `ERROR: MethodError: no method matching MultiPolygon(::Point{2, Float32})`
     for (c, mandate_year) in zip(eu_geojson, mandate_year_vect)
-        poly!(ga, GeoMakie.geo2basic(c);
-            strokecolor = RGBf(0.90, 0.90, 0.90),
-            strokewidth = 1,
-            color= color_scale_[mandate_year - 2019],
-            label=string(mandate_year)
+        poly!(
+            ga,
+            GeoMakie.geo2basic(c);
+            strokecolor=RGBf(0.90, 0.90, 0.90),
+            strokewidth=1,
+            color=color_scale_[mandate_year - 2019],
+            label=string(mandate_year),
         )
     end
 
-    axislegend(ga, merge=true)
+    axislegend(ga; merge=true)
 
     # cbar = Colorbar(gd[1,2]; colormap = color_scale_, label = "ESEF Reports (all-time, per country)", height = Relative(0.65))
 
@@ -242,22 +261,24 @@ function generate_esef_homepage_viz(; map_output="web")
     # jscpd:ignore-start
     viz["esef_country_availability_map"] = generate_esef_report_map()
 
-
     axis = (
         width=500,
         height=250,
         xlabel="",
         ylabel="Report Count",
         title="ESEF Report Availability by Country",
-        subtitle = "(XBRL Repository)",
-        xticklabelrotation = pi/2,
+        subtitle="(XBRL Repository)",
+        xticklabelrotation=pi / 2,
     )
 
     country_ordered = country_rollup[!, :country]
-    
+
     plt = @chain country_rollup begin
         data(_) *
-        mapping(:country => renamer((zip(country_ordered, country_ordered) |> OrderedDict)...), :report_count) *
+        mapping(
+            :country => renamer((OrderedDict(zip(country_ordered, country_ordered)))...),
+            :report_count,
+        ) *
         visual(BarPlot; color=trr_266_colors[1])
     end
 
