93522e822a81228952ba41314ed81ca86ef15366
Jeremiah <4462211+jeremiahpslewis@users.noreply.github.com>
2022-12-09 20:54:03 +0000

    Drop xz

--- src/local_sparql_db/oxigraph_server.jl
+++ src/local_sparql_db/oxigraph_server.jl
@@ -2,7 +2,6 @@ using HTTP
 using JSON
 using Chain
 using Arrow
-using XZ_jll
 
 function serve_oxigraph(;
     nt_file_path="", db_path=".cache/esef_oxigraph_data", rebuild_db=true, keep_open=false
@@ -22,23 +21,23 @@ function serve_oxigraph(;
     # 2. Download rdf triples data 
     if nt_file_path == ""
         rm(".qlever"; force=true, recursive=true)
-        run(`git clone https://github.com/ad-freiburg/qlever .qlever`)
-        run(`$(xz()) -d .cache/.qlever/examples/olympics.nt.xz`)
+        run(`git clone https://github.com/ad-freiburg/qlever .cache/.qlever`)
+        run(`xz -d .cache/.qlever/examples/olympics.nt.xz`)
         nt_file_path = ".cache/.qlever/examples/olympics.nt"
     end
 
-    # 2. Load data into database
+    # 3. Load data into database
     run(
         `$(ENV["HOME"])/.cargo/bin/oxigraph_server --location $db_path load --file $nt_file_path`,
     )
 
-    # 3. Spin up database
+    # 4. Spin up database
     oxigraph_process = run(
         `$(ENV["HOME"])/.cargo/bin/oxigraph_server --location $db_path serve`; wait=false
     )
 
     try
-        # 4. Test query database
+        # 5. Test query database
         q_path = joinpath(
             @__DIR__, "..", "..", "queries", "local", "local_query_test.sparql"
         )
@@ -46,14 +45,14 @@ function serve_oxigraph(;
 
         n_items = @chain df[!, "count"][1]["value"] parse(Int64, _)
 
-        # 5. Check that we got the right number of items
+        # 6. Check that we got the right number of items
         @assert n_items == countlines(nt_file_path)
     catch
         @assert n_items == countlines(nt_file_path),
         "Basic integrity check failed, check whether dataset has duplicates!"
         kill(oxigraph_process)
     finally
-        # 6. Stop database
+        # 7. Stop database
         if keep_open
             return oxigraph_process
         else
