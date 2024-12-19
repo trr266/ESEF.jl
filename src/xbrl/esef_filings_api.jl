4b54a68e751ad12873a9c73b042e3c02ed47b135
Jeremiah Lewis <4462211+jeremiahpslewis@users.noreply.github.com>
2024-12-19 16:38:38 +0100

    start refactor for xbrl api

--- src/xbrl/esef_filings_api.jl
+++ src/xbrl/esef_filings_api.jl
@@ -44,11 +44,10 @@ end
 # TODO: Extract XBRL facts from items where "xbrl-json" key is populated.
 # 2594003JTXPYO8NOG018/2020-12-31/ESEF/PL/0
 # https://filings.xbrl.org/2594003JTXPYO8NOG018/2020-12-31/ESEF/PL/0/enea-2020-12-31.json
-
-@memoize function get_esef_xbrl_filings()
-    xbrl_esef_index_endpoint = "https://filings.xbrl.org/index.json"
+function get_esef_xbrl_filings(page_num)
+    xbrl_esef_index_endpoint = "https://filings.xbrl.org/api/filings?page[size]=200&page[number]=$page_num"
     r = HTTP.get(xbrl_esef_index_endpoint)
-
+    
     # Check 200 HTTP status code
     @assert(r.status == 200)
 
@@ -65,28 +64,27 @@ end
         :date,
         :filing_key,
         :error_count,
-        :error_codes,
         :xbrl_json_path,
     )
 
     df_error = DataFrame()
 
     # Parse XBRL ESEF Index Object
-    for (d_key, d_value) in raw_data
-        entity_name = d_value["entity"]["name"]
+    for d_value in raw_data["data"]
+        entity_name = split(d_value["relationships"]["entity"]["links"]["related"], "/")[end]
 
-        for (filing_key, filing_value) in d_value["filings"]
-            error_payload = filing_value["errors"]
-            error_count = length(error_payload)
-            error_codes = [d["code"] for d in error_payload]
+        for attributes in d_value["attributes"]
+            filing_key = attributes["fxo_id"]
+            error_count = attributes["error_count"]
 
-            country = filing_value["country"]
-            date = filing_value["date"]
+            country = attributes["country"]
+            date = attributes["period_end"]
 
             xbrl_json_path = nothing
 
-            if haskey(filing_value, "xbrl-json")
-                xbrl_json_path = filing_value["xbrl-json"]
+            # TODO: Figure out why this errors / make missing-field tolerant
+            if haskey(attributes, "json_url")
+                xbrl_json_path = attributes["json_url"]
                 xbrl_json_path = xbrl_json_path == "" ? nothing : xbrl_json_path
             end
 
@@ -97,14 +95,9 @@ end
                 date,
                 filing_key,
                 error_count,
-                error_codes,
                 xbrl_json_path,
             ])
             push!(df, new_row; promote=true)
-
-            for error_code in error_codes
-                push!(df_error, NamedTuple{(:key, :error_code)}([d_key, error_code]))
-            end
         end
     end
 
@@ -123,7 +116,23 @@ end
         leftjoin(_, country_lookup; on=:country_alpha_2)
     end
 
-    return df, df_error
+    return df
+end
+
+@memoize function get_esef_xbrl_filings()
+    # NOTE: use   "links"   => Dict{String, Any}("next"=>"https://filings.xbrl.org/api/filings?page%5Bsize%5D=200&page%5Bnumber%5D=2", to iterate through api
+    df = DataFrame()
+
+    for i in 1:100
+        df_ = get_esef_xbrl_filings(i)
+
+        while nrow(df_) == 200
+            append!(df, df_)
+            i += 1
+        end
+
+        append!(df, df_)
+    end
 end
 
 function calculate_country_rollup(df)
