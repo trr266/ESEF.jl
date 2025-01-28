142ef3019d8592cc4bcd50d6db81c45fe01fc6d9
Jeremiah Lewis <4462211+jeremiahpslewis@users.noreply.github.com>
2025-01-28 13:39:33 +0100

    Add filter for sovereign countries in SPARQL query and improve country ISO2 lookup function

--- src/wikidata/iso_country_table.jl
+++ src/wikidata/iso_country_table.jl
@@ -1,12 +1,15 @@
 using Chain
 using Memoization
 using DataFrameMacros
+using DataFrames
 
 @memoize function get_wikidata_country_iso2_lookup()
     q_path = joinpath(@__DIR__, "..", "..", "queries", "wikidata", "country_iso_2.sparql")
-    @chain q_path begin
+    df = @chain q_path begin
         query_wikidata_sparql()
         unpack_value_cols([:country, :country_alpha_2, :countryLabel])
         strip_wikidata_prefix([:country])
     end
+    @assert length(unique(df[!, :country_alpha_2])) == nrow(df)
+    return df
 end
