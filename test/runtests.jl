8d2bcb4dc3a2904816405171895e562c61e5b522
Jeremiah Lewis <4462211+jeremiahpslewis@users.noreply.github.com>
2025-01-28 15:51:28 +0100

    Reorganize ESEF.jl Visualizations test set for improved clarity and consistency

--- test/runtests.jl
+++ test/runtests.jl
@@ -73,21 +73,6 @@ lei_list = [lei, "HWUPKR0MPOU8FGXBT394"]
     @test names(country_rollup) == ["countryLabel", "report_count"]
 end
 
-@testset "ESEF.jl Visualizations" begin
-    plots = generate_esef_homepage_viz(debug=true)
-
-    # Check all plots generated
-    @test sort([keys(plots)...]) == sort([
-        :esef_country_availability_bar,
-        :esef_country_availability_map,
-        :esef_error_country_heatmap,
-        :esef_error_hist,
-        :esef_error_type_freq_bar,
-        :esef_mandate_overview,
-        :esef_publication_date_composite,
-    ])
-end
-
 @testset "oxigraph db load" begin
     serve_oxigraph(rebuild_db = true)
 end
@@ -449,3 +434,19 @@ end
         "error_code"
     ]
 end
+
+@testset "ESEF.jl Visualizations" begin
+    plots = generate_esef_homepage_viz(debug=true)
+
+    # Check all plots generated
+    @test sort([keys(plots)...]) == sort([
+        :esef_country_availability_bar,
+        :esef_country_availability_map,
+        :esef_error_country_heatmap,
+        :esef_error_hist,
+        :esef_error_type_freq_bar,
+        :esef_mandate_overview,
+        :esef_publication_date_composite,
+    ])
+end
+
