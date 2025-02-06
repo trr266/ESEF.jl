bc64866134ea96fdc7b047a8ac547682583c29f7
Jeremiah Lewis <4462211+jeremiahpslewis@users.noreply.github.com>
2025-02-06 12:28:22 +0100

    Rename variables for clarity in error message heatmap generation

--- src/exploration/visualizations.jl
+++ src/exploration/visualizations.jl
@@ -243,12 +243,11 @@ function generate_esef_error_type_freq_bar(; debug=false)
 end
 
 function generate_esef_error_country_heatmap(; debug=false)
-    df_error_wide = get_error_messages(debug=debug)
+    df_esef_error = get_error_messages(debug=debug)
     df_xbrl_raw = get_esef_xbrl_filings(debug=debug)
-    df_esef_error = leftjoin(df_xbrl_raw, df_esef_error, on=:error_json_path)
+    df_error_wide = leftjoin(df_xbrl_raw, df_esef_error, on=:error_json_path)
 
     df_error_country = @chain df_error_wide begin
-        @transform(:error_code = truncate_text(:error_code))
         @groupby(:error_code, :countryLabel)
         @combine(:error_count = length(:error_code))
         @sort(-:error_count)
